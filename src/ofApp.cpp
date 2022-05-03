b6ebdc01134b356ab89134c1c1275e1c9858be68
sari <>
2022-05-03 14:35:23 +0900

    succeeded in integrating each programs

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,17 +2,44 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    //スタンドアローンビルドのためのおまじない
+    ofSetDataPathRoot("../Resources/data/");
+    ofSetFrameRate(5);    //フレームレート指定
 
+    ssd.init(CAM_W, CAM_H, CAM_R);    //顔検出用
+    camdiff.init(CAM_W, CAM_H, CAM_R);//webcam輝度用
+
+    aw.init();    //aw情報を初期化
+    sc.init();    //scの初期化
+
+    //イベント情報を初期化
+    //マウスイベント，キーイベント管理クラス初期化~イベントタップ有効化・ランループ実行までを一括実行
+    event.init();
+    
+    //開始時刻を記録
+    old = ofGetElapsedTimef();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    ssd.update();       //顔検出を更新
+    camdiff.update();   //webcam輝度差分を更新
 
+    aw.update();        //aw情報を更新
+    sc.update();        //スクショ情報を更新
+
+    //eventのみ，コールバック関数内部でupdateを定義
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    if(isPassed(&old)){
+        ssd.clear();
+        camdiff.clear();
+        sc.clear();
+        aw.clear();
+        event.clear();
+    }
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,6 +2,26 @@
 
 #include "ofMain.h"
 
+//for Webcam-info
+#include "FaceDetect.hpp"
+#include "WebcamDiff.hpp"
+#include "ofxGraph.h"   //for Graph
+#include "ofxCsv.h"     //for Csv
+
+//for Display-info
+#include "ActiveWindow.hpp"
+#include "ScreenGrab.hpp"
+
+//for Operation-info
+#include "myEvents.hpp"
+
+#define INTERVAL 5
+
+//for Webcam
+#define CAM_W 1920
+#define CAM_H 1080
+#define CAM_R 3
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -20,5 +40,22 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+
+    FaceDetect ssd;     //顔検出を管理するクラス
+    WebcamDiff camdiff; //輝度差分抽出を管理するクラス
+    ActiveWindow aw;    //アクティブウィンドウを示すクラス
+    ScreenGrab sc;      //スクショ管理用のクラス
+    myEvents event;     //イベント管理用のクラス
+
+    int old;            //時刻を格納しておく変数
+
+    //指定時間が経過したかどうかを判断する論理関数
+    bool isPassed(int* time){
+        if((int)ofGetElapsedTimef()-(*time)==INTERVAL){
+            *time = ofGetElapsedTimef();
+            return true;
+        }
+        else
+            return false;
+    };
 };
